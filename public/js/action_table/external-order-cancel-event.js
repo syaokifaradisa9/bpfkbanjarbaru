68491b4ea48926d969a497f2ccc30b5fe5afe686
Muhammad Syaoki Faradisa <syaokifaradisa09@gmail.com>
2022-06-13 15:55:59 +0800

    Penambahan Script Batal Oleh Fasyenkes

---
+++ public/js/action_table/external-order-cancel-event.js
@@ -0,0 +1,63 @@
+async function cancelExternalOrder(event){
+    event.preventDefault();
+
+    var dataId = event.target.id.split('-')[2];
+    var order_id = document.getElementById(`order-id-${dataId}`).innerHTML.trim();
+
+    const swalWithBootstrapButtons = Swal.mixin({
+        customClass: {
+          confirmButton: 'btn btn-success',
+          cancelButton: 'btn btn-danger'
+        },
+        buttonsStyling: false
+      })
+      
+      swalWithBootstrapButtons.fire({
+        title: 'Konfirmasi Pembatalan Order External?',
+        text: "Yakin ingin Membatalkan Order External ini?",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonText: 'Ya',
+        cancelButtonText: 'Batal',
+        reverseButtons: true
+      }).then(async (result) => {
+        if (result.isConfirmed) {
+          const response = await fetch(
+              `http://bpfkbanjarbaru.test/order/external/${order_id}/cancel`,
+              {
+                method: "GET",
+                headers: {'Content-Type': 'application/json'}
+              }
+          );
+
+          var json = await response.json();
+          if(json.status == 'success'){
+            document.getElementById(`status_${dataId}`).innerHTML = "DIBATALKAN";
+            document.getElementById(`btn-cancel-${dataId}`).classList.add('d-none');
+            document.getElementById(`btn-edit-order-${dataId}`).classList.add('d-none');
+
+            Swal.fire({
+              icon: 'success',
+              title: 'Sukses',
+              text: 'Sukses Membatalkan Order External'
+            });
+          }else{
+            Swal.fire({
+              icon: 'error',
+              title: 'Gagal!',
+              text: 'Gagal Membatalkan Order External, Silahkan Coba Lagi!'
+            });
+          }
+        }
+    });
+}
+
+document.addEventListener("DOMContentLoaded", function(){
+    const orderTable = document.getElementById('external-order-table');
+    const totalDataRow = orderTable.rows.length - 1;
+
+    for(var i=0; i<totalDataRow; i++){
+        var cancelButton = document.getElementById(`btn-cancel-${i}`);
+        cancelButton.addEventListener('click', cancelExternalOrder);
+    }
+});
